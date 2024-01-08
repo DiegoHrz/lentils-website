d8982181f1ab146feaeb64fc252184f3287620ad
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-07 23:00:00 -0500

    Add Link in Navbar

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -1,14 +1,26 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div className=" flex justify-between m-10 bg-stone-700">
+    <div className=" flex justify-between p-7 bg-stone-700">
       <div>Lentejas Sol del Norte</div>
       <div>
         <ul className="flex mr-3">
-          <li className="mr-20">Inicio</li>
-          <li className="mr-20">Historia</li>
-          <li className="mr-20">Productos</li>
-          <li className="mr-20">Contacto</li>
-          <li className="mr-10">Recetas y Cocina</li>
+          <Link href="/">
+            <li className="mr-20">Inicio</li>
+          </Link>
+          <Link href="">
+            <li className="mr-20">Historia</li>
+          </Link>
+          <Link href="">
+            <li className="mr-20">Productos</li>
+          </Link>
+          <Link href="">
+            <li className="mr-20">Contacto</li>
+          </Link>
+          <Link href="">
+            <li className="mr-10">Recetas y Cocina</li>
+          </Link>
         </ul>
       </div>
     </div>
---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div>
+      <img src="./lentils.webp" alt="lentils" />
+    </div>
+  );
+};
+
+export default Banner;
