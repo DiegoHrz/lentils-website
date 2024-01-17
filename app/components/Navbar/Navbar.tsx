3c8b0234ee1158e0cb9992ecebbecc11cf9a1e7d
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-16 22:55:53 -0500

    Add changes without rootlayout

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 "use client";
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-const Navbar = () => {
+
+
+const Navbar: React.FC = () => {
   // State para el scroll
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
 
@@ -22,73 +25,74 @@ const Navbar = () => {
     };
   }, []);
 
+
   const navbarStyle = isScrolled
     ? " bg-white text-brown text-amber-800"
     : "bg-transparent bg-opacity-95 hover:bg-white hover:text-brown hover:text-amber-800";
 
   return (
-      <div className="fixed z-50">
-        <div className="flex justify-center items-center bg-yellow-900 w-screen font-light text-xs p-2">
-          Envios a toda Latinoamerica , Canada y USA.
+    <div className="fixed z-50">
+      <div className="flex justify-center items-center bg-yellow-900 w-screen font-light text-xs p-2">
+        Envios a toda Latinoamerica , Canada y USA.
+      </div>
+      <div
+        className={`flex justify-between p-3 w-screen fixed ${navbarStyle} bg-opacity-95 m-0 `}
+      >
+        <div className="flex items-center m-0">
+          <div className="flex flex-col justify-center items-center text-center text-lg font-medium hover:cursor-pointer  h-fit m-0">
+            <h1>
+              <Link href="" className="text-sm">
+                LENTEJAS DEL SOL DEL NORTE
+              </Link>
+            </h1>
+          </div>
         </div>
-        <div
-          className={`flex justify-between p-3 w-screen fixed ${navbarStyle} bg-opacity-95 m-0 `}
-        >
-          <div className="flex items-center m-0">
-            <div className="flex flex-col justify-center items-center text-center text-lg font-medium hover:cursor-pointer  h-fit m-0">
-              <h1>
-                <Link href="" className="text-sm">
-                  LENTEJAS DEL SOL DEL NORTE
-                </Link>
-              </h1>
-            </div>
-          </div>
-          <div className=" m-0">
-            <ul className="flex  mr-3 ">
-              <Link
-                href="/"
-                className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20"
-              >
-                <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
-                  INICIO
-                </li>
-              </Link>
-              <Link
-                href="/historia"
-                className="group rounded-lg border border-transparent transition-colors hover:underline mr-20"
-              >
-                <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
-                  HISTORIA
-                </li>
-              </Link>
-              <Link
-                href="/productos"
-                className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20 hover:border-b-text-sky-600"
-              >
-                <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
-                  PRODUCTOS
-                </li>
-              </Link>
-              <Link
-                href="/contacto"
-                className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20"
-              >
-                <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
-                  CONTACTO
-                </li>
-              </Link>
-              <Link
-                href="/recetas"
-                className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20"
-              >
-                <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
-                  RECETAS Y COCINA
-                </li>
-              </Link>
-            </ul>
-          </div>
+        <div className=" m-0">
+          <ul className="flex  mr-3 ">
+            <Link
+              href="/"
+              className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20"
+            >
+              <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
+                INICIO
+              </li>
+            </Link>
+            <Link
+              href="/historia"
+              className="group rounded-lg border border-transparent transition-colors hover:underline mr-20"
+            >
+              <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
+                HISTORIA
+              </li>
+            </Link>
+            <Link
+              href="/productos"
+              className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20 hover:border-b-text-sky-600"
+            >
+              <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
+                PRODUCTOS
+              </li>
+            </Link>
+            <Link
+              href="/contacto"
+              className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20"
+            >
+              <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
+                CONTACTO
+              </li>
+            </Link>
+            <Link
+              href="/recetas"
+              className="group rounded-lg border border-transparent transition-colors  hover:underline mr-20"
+            >
+              <li className="mt-3  mb-3  font-medium text-sm hover:text-amber-950">
+                RECETAS Y COCINA
+              </li>
+            </Link>
+          </ul>
         </div>
       </div>
+    </div>
   );
 };
 
--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,7 +1,7 @@
 const Contacto = () => {
   return (
     <div className="w-screen h-screen">
-      <div className="w-screen h-screen">
+      <div className="w-screen h-screen text-red-800">
         <div>Contactanos</div>
       </div>
     </div>
