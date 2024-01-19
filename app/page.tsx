537ffbd7d0ee4645a8f4820b828341f4f8bd38ec
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-18 23:59:45 -0500

    Add padding style in Home

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   // }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between ">
       {/* Inicio */}
       {/* <div className="-mt-24 w-screen h-screen">
         <img
