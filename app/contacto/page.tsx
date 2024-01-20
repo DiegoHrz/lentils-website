e967eb7a94f649db9d9bc9a8d0f42a40c7592811
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-19 23:36:32 -0500

    Add styles in title and subtittle in contact.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,14 +1,20 @@
 const Contacto = () => {
   return (
     <div className="">
-      <div>
-        <p>Contactanos</p>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-4xl font-semibold m-6">
+          <p>Contactanos</p>
+        </div>
+        <div className="m-6 font-serif text-2xl">
+          Encantados de contactar contigo en todo momento
+        </div>
+        <div className="bg-white w-full text-yellow-900 flex justify-center m-6 italic font-normal text-base mb-10">
+          Datos de contacto Importaciones Dionys Representado por Intercompany &
+          Sr de Huánca Disponibles para consultas, compras y solicitudes de
+          cotizaciones
+        </div>
       </div>
-      <div>
-        Datos de contacto Importaciones Dionys Representado por Intercompany &
-        Sr de Huánca Disponibles para consultas, compras y solicitudes de
-        cotizaciones
-      </div>
+
       <div className="flex" style={{ height: "75vh" }}>
         <div className="w-1/2">
           <div>
