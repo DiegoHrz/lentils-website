d0b2bc7eae118bdd15b8c227c1ebad3785dc493e
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-19 23:23:26 -0500

    Add info en contacto.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,12 +1,15 @@
 const Contacto = () => {
   return (
-    <div>
+    <div className="">
       <div>
         <p>Contactanos</p>
       </div>
-      <div>Descripcion de contactanos</div>
-      <div className="" style={{ height: "75vh" }}>
-        form
+      <div>
+        Datos de contacto Importaciones Dionys Representado por Intercompany &
+        Sr de Huánca Disponibles para consultas, compras y solicitudes de
+        cotizaciones
+      </div>
+      <div className="flex" style={{ height: "75vh" }}>
         <div className="w-1/2">
           <div>
             <div>
@@ -29,15 +32,39 @@ const Contacto = () => {
                 </div>
                 <div className="flex flex-col">
                   <label htmlFor="">Mensaje</label>
-                  <input type="text" name="" id="" />
+                  <textarea placeholder="Escribenos"></textarea>
+                </div>
+                <div className="border">
+                  <input type="submit" value="Enviar" />
                 </div>
               </form>
             </div>
           </div>
         </div>
-        <div className="w-1/2"></div>
+        <div className="w-1/2">
+          <div>
+            <div>Contactanos</div>
+            <div>
+              <div>Telefono</div>
+              <div>whatsapp</div>
+              <div>Oficinas</div>
+              <div>Correo de ventas</div>
+              <div></div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>Dudas o preguntas</div>
+      <div className="h-screen">
+        <div>
+          <div>¿ Tienes dudas o preguntas ?</div>
+          <div>
+            Puedes llamarnos a los siguientes número telefónicos (+51) 1 323
+            8817 de nuestra tienda principal en lima o envíanos un email a
+            ventas@grupodionys.com.pe donde también podrás solicitar
+            cotizaciones
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
