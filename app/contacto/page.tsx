e84b60e03c1d5485e916921ee0bac4bf02395519
DiegoHrz <hernandezwdiego@gmail.com>
2024-01-21 23:59:11 -0500

    Add info in contacto section in contacto.ts

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,11 +1,11 @@
 const Contacto = () => {
   return (
-    <div className="">
+    <div className="bg-white">
       <div className="flex flex-col justify-center items-center">
-        <div className="text-4xl font-semibold m-6">
+        <div className="text-yellow-900 text-5xl font-medium mt-14 ">
           <p>Contactanos</p>
         </div>
-        <div className="m-6 font-serif text-2xl">
+        <div className="m-6 font-serif text-xl text-neutral-500">
           Encantados de contactar contigo en todo momento
         </div>
         <div className="bg-white w-full text-yellow-900 flex justify-center m-6 italic font-normal text-base mb-10">
@@ -15,12 +15,30 @@ const Contacto = () => {
         </div>
       </div>
 
-      <div className="flex border mx-14" style={{ height: "100vh" }}>
-        <div className="w-3/5">
-          <div className="">
+      {/* <h1 className="text-yellow-900 text-5xl font-medium ">Nutricion</h1>
+            <p className="pt-3 text-xl font-normal text-neutral-500 ">
+              Descubre los beneficios nutricionales de nuestras lentejas: ricas
+              en proteínas, fibra y nutrientes esenciales. ¡Bienestar natural
+              con Sol del Norte!
+            </p>
+            <br />
+            <p className="  italic">Conoce como prepararlas:</p>
+            <p>
+              <Link
+                href="/recetas"
+                className="text-sky-600 cursor-pointer hover:underline"
+              >
+                Recetas y Cocina ➤
+              </Link>
+            </p>
+          </div> */}
+
+      <div className="flex  rounded-lg mx-14" style={{ height: "100vh" }}>
+        <div className="w-3/5 bg-neutral-50  rounded-2xl">
+          <div className="  rounded-lg">
             <div className="m-10 ">
               <form action="" className="">
-                <div className="flex flex-col border mx-3 my-5  rounded-lg">
+                <div className="flex flex-col border mx-3 my-5  rounded-lg bg-white text-black">
                   <label htmlFor="" className="m-3 text-sm">
                     {" "}
                     Nombre Completo
@@ -29,13 +47,13 @@ const Contacto = () => {
                     type="text"
                     name=""
                     id=""
-                    className="bg-black m-3"
+                    className=" m-3"
                     placeholder="Ingresa tu Nombre"
                   />
                 </div>
                 <div className="flex">
-                  <div className="flex flex-col border mx-3 my-5 rounded-lg w-1/2">
-                    <label htmlFor="" className="m-3 text-sm">
+                  <div className="flex flex-col border mx-3 my-5 rounded-lg w-1/2 bg-white text-black">
+                    <label htmlFor="" className="m-3 text-sm ">
                       Correo Electronico
                     </label>
                     <input
@@ -43,11 +61,11 @@ const Contacto = () => {
                       name=""
                       id=""
                       placeholder="Ingresa tu Correo"
-                      className="bg-black m-3"
+                      className=" m-3"
                     />
                   </div>
-                  <div className="flex flex-col border mx-3 my-5 rounded-lg w-1/2">
-                    <label htmlFor="" className="m-3 text-sm">
+                  <div className="flex flex-col border mx-3 my-5 rounded-lg w-1/2 bg-white text-black">
+                    <label htmlFor="" className="m-3 text-sm ">
                       Celular
                     </label>
                     <input
@@ -55,12 +73,12 @@ const Contacto = () => {
                       name=""
                       id=""
                       placeholder="Ingresa tu Numero"
-                      className="bg-black m-3"
+                      className=" m-3"
                     />
                   </div>
                 </div>
-                <div className="flex flex-col border mx-3 my-5 rounded-lg">
-                  <label htmlFor="" className="m-3 text-sm">
+                <div className="flex flex-col border mx-3 my-5 rounded-lg bg-white text-black">
+                  <label htmlFor="" className="m-3 text-sm ">
                     Asunto
                   </label>
                   <input
@@ -68,32 +86,34 @@ const Contacto = () => {
                     name=""
                     id=""
                     placeholder="Ingresa el Asunto"
-                    className="bg-black m-3"
+                    className=" m-3"
                   />
                 </div>
-                <div className="flex flex-col border mx-3 my-10 rounded-lg">
-                  <label htmlFor="" className="m-3 text-sm">
+                <div className="flex flex-col border mx-3 my-10 rounded-lg bg-white text-black">
+                  <label htmlFor="" className="m-3 text-sm ">
                     Mensaje
                   </label>
                   <textarea
                     placeholder="Escribenos"
-                    className="bg-black m-3"
+                    className=" m-3"
                   ></textarea>
                 </div>
                 <div className="mx-3 my-6">
                   <div>
                     <input type="checkbox" name="" id="" />{" "}
-                    <label htmlFor="">Aceptar Teminos y Condiciones</label>
+                    <label htmlFor="" className="text-black text-sm">
+                      Aceptar Teminos y Condiciones
+                    </label>
                   </div>
                   <div>
                     <input type="checkbox" name="" id="" />{" "}
-                    <label htmlFor="">
+                    <label htmlFor="" className="text-black text-sm">
                       Aceptar recibir notificaciones de actualizaciones en
                       novedades
                     </label>
                   </div>
                 </div>
-                <div className="border   flex justify-center items-center rounded-lg mx-3">
+                <div className="border   flex justify-center items-center rounded-lg mx-3 bg-white text-black">
                   <input type="submit" value="Enviar" className="" />
                 </div>
               </form>
@@ -101,8 +121,8 @@ const Contacto = () => {
           </div>
         </div>
         <div className="w-2/5">
-          <div className="border h-full flex justify-center items-start">
-            <div className="border ">
+          <div className=" h-full flex justify-center items-start">
+            <div className=" ">
               <div className="text-yellow-900 text-4xl font-medium  flex m-10">
                 Contactanos
               </div>
@@ -110,18 +130,18 @@ const Contacto = () => {
                 Listos para resolver todas tus dudas y hacer ventas contigo en
                 todo momento
               </div>
-              <div className="ml-20">
-                <div className="my-24">
-                  <a href="">Telefono: (01) 5050505</a>
+              <div className="ml-20 text-black">
+                <div className="my-20">
+                  <a href="" className="flex justify-start items-center"><div className=" border rounded-full text-lg">☎️</div>  Telefono: (01) 5050505</a>
                 </div>
-                <div className="my-24">
-                  <a href="">Whatsapp: +51 974377347</a>
+                <div className="my-20">
+                  <a href="" className="flex justify-start items-center"> <div className=" border rounded-full">✆</div> Whatsapp: +51 974377347</a>
                 </div>
-                <div className="my-24">
-                  <a href="">Oficinas: Av Siempreviva 241</a>
+                <div className="my-20">
+                  <a href="" className="flex justify-start items-center"><div className=" border rounded-full">📍</div>Oficinas: Av Siempreviva 241</a>
                 </div>
-                <div className="my-24">
-                  <a href="">Correo: tusventasfavoritas@gmail.com</a>
+                <div className="my-20">
+                  <a href="" className="flex justify-start items-center"><div className=" border rounded-full">✉️</div>Correo: tusventasfavoritas@gmail.com</a>
                 </div>
               </div>
             </div>
