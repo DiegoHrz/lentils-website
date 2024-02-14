f08ef1b9e964d463e829ea8ecc90234a2c55ebb7
DiegoHrz <hernandezwdiego@gmail.com>
2024-02-13 23:48:20 -0500

    Add changes in receta

--- client/app/recetas/page.tsx
+++ client/app/recetas/page.tsx
@@ -2,7 +2,7 @@ const Recetas = () => {
   return (
     <div className="w-screen h-screen">
       <div className="w-screen h-screen">
-        <div>Recetas INTENSAS</div>
+        <div className="text-3xl">Recetas INTENSAS</div>
       </div>
     </div>
   );
