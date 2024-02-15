0005af7c040e439e85b22738b8b16018cf79e733
DiegoHrz <hernandezwdiego@gmail.com>
2024-02-14 23:33:23 -0500

    Add historia header

--- client/app/historia/page.tsx
+++ client/app/historia/page.tsx
@@ -2,7 +2,7 @@ const Historia = () => {
   return (
     <div className="w-screen h-screen">
       <div className="w-screen h-screen">
-        <div>Nuestra Historia</div>
+        <div>Nuestra Historia al alcance de tus manos</div>
       </div>
     </div>
   );
