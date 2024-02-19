6283fb54af037f11f5748ceac67ffb69ac138430
DiegoHrz <hernandezwdiego@gmail.com>
2024-02-18 23:59:13 -0500

    Add historia he

--- client/app/recetas/page.tsx
+++ client/app/recetas/page.tsx
@@ -2,7 +2,7 @@ const Recetas = () => {
   return (
     <div className="w-screen h-screen">
       <div className="w-screen h-screen">
-        <div className="text-3xl">Recetas INTENSAS c</div>
+        <div className="text-3xl">Recetas INTENSAS </div>
       </div>
     </div>
   );
