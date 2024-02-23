adc70cb1e06edb24792adc31e11b0f015f3d0292
DiegoHrz <hernandezwdiego@gmail.com>
2024-02-22 23:57:42 -0500

    Add responsive historia info

--- app/historia/page.tsx
+++ app/historia/page.tsx
@@ -3,6 +3,8 @@ const Historia = () => {
     <div className="w-screen h-screen">
       <div className="w-screen h-screen">
         <div>Nuestra Historia al alcance de tus manos</div>
+        <div>Aca le contamos la ardua tarea de brindar las mejores lentejas a la meza de cada peruano</div>
+
       </div>
     </div>
   );
