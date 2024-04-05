b1ed15993b3fe3e398f2a40429ca370e69dc9ca5
DiegoHrz <hernandezwdiego@gmail.com>
2024-04-04 23:50:02 -0500

    Add d

--- app/historia/page.tsx
+++ app/historia/page.tsx
@@ -4,7 +4,7 @@ const Historia = () => {
       <div className="w-screen h-screen">
         <div>Nuestra Historia al alcance de tus manos</div>
         <div>Aca le contamos la ardua tarea de brindar las mejores lentejas a la meza de cada peruano</div>
-        <div>Aca le contamos la ardua tarea de brindar las mejores lentejas a la meza de cada peruano</div>
+        <div>Empezamos desde pequeños sin tener conocimientos de nada y somos dioses jeje</div>
 
       </div>
     </div>
