1420dd8b743cd971b26cf4dac6d719aaa3b37a40
DiegoHrz <hernandezwdiego@gmail.com>
2024-05-01 23:55:15 -0500

    ADD

--- app/historia/page.tsx
+++ app/historia/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+import { useEffect, useState } from "react";
+
 const Historia = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log(counter);
+    return () => {
+      console.log("counter died");
+    };
+  }, [counter]);
+
   return (
     <div className="w-screen h-screen">
       <div className="w-screen h-screen">
         <div>Nuestra Historia al alcance de tus manos</div>
-        <div>Aca le contamos la ardua tarea de brindar las mejores lentejas a la meza de cada peruano</div>
-        <div>Empezamos desde pequeños sin tener conocimientos de nada y somos dioses jeje</div>
-
+        <div>
+          Aca le contamos la ardua tarea de brindar las mejores lentejas a la
+          meza de cada peruano
+        </div>
+        <div>
+          Empezamos desde pequeños sin tener conocimientos de nada y somos
+          dioses jeje
+          <div>yes</div>
+        </div>
+        <button
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button>
+        <div>{counter}</div>
       </div>
     </div>
   );
